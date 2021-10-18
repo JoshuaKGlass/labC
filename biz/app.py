96c3531c6f13633ab31ee75cdc972eed45fc069f
Joshua Glass <jkglass@hotmail.co.uk>
2021-10-18 19:46:58 +0100

    step 1 of lab

--- biz/app.py
+++ biz/app.py
@@ -8,10 +8,37 @@ client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.bizDB
 businesses = db.biz
 
+
+@app.route("/api/v1.0/businesses", methods=["GET"])
+def show_all_businesses():
+    page_num, page_size = 1, 10
+    if request.args.get('pn'):
+        page_num = int(request.args.get('pn'))
+    if request.args.get('ps'):
+        page_size = int(request.args.get('ps'))
+    page_start = (page_size * (page_num - 1))
+
+    data_to_return = []
+    for business in businesses.find().skip(page_start).limit(page_size):
+        business['_id'] = str(business['_id'])
+        for review in business['reviews']:
+            review['_id'] = str(review['_id'])
+        data_to_return.append(business)
+
+    return make_response(jsonify(data_to_return), 200)
+
+
+@app.route("/api/v1.0/businesses/<string:id>", methods=["GET"])
+def show_one_business(id):
+    business = businesses.find_one({'_id': ObjectId(id)})
+    if business is not None:
+        business['_id'] = str(business['_id'])
+        for review in business['reviews']:
+            review['_id'] = str(review['_id'])
+        return make_response(jsonify(business), 200)
+    else:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
